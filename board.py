626b5d95a8491a05eb5b88a9d862214bd5877ebc
Milo Park <milo@nat-wireless-guest-reg-153-53.bu.edu>
2022-04-01 00:27:48 -0400

    Create heuristic function to solve puzzle

---
+++ board.py
@@ -0,0 +1,158 @@
+#
+# board.py (Final project)
+#
+# A Board class for the Eight Puzzle
+#
+# name: Min Sung Park
+# email: minpark@bu.edu
+#
+# If you worked with a partner, put their contact info below:
+# partner's name:
+# partner's email:
+#
+
+# a 2-D list that corresponds to the tiles in the goal state
+GOAL_TILES = [['0', '1', '2'],
+              ['3', '4', '5'],
+              ['6', '7', '8']]
+
+class Board:
+    """ A class for objects that represent an Eight Puzzle board.
+    """
+    def __init__(self, digitstr):
+        """ a constructor for a Board object whose configuration
+            is specified by the input digitstr
+            input: digitstr is a permutation of the digits 0-9
+        """
+        # check that digitstr is 9-character string
+        # containing all digits from 0-9
+        assert(len(digitstr) == 9)
+        for x in range(9):
+            assert(str(x) in digitstr)
+
+        self.tiles = [[''] * 3 for x in range(3)]
+        self.blank_r = -1
+        self.blank_c = -1
+
+        # Put your code for the rest of __init__ below.
+        # Do *NOT* remove our code above.
+        
+        for r in range(3):
+            for c in range(3):
+                self.tiles[r][c] = (digitstr[3*r + c])
+                if self.tiles[r][c] == '0':
+                    self.blank_r = r
+                    self.blank_c = c
+                    
+
+    ### Add your other method definitions below. ###
+    
+    def __repr__(self):
+        '''returns a string representation of a Board object
+        '''
+        nump = ''
+        for r in range(3):
+            for c in range(3):
+                if self.tiles[r][c] != '0':
+                    nump += self.tiles[r][c] + ' '
+                else:
+                    nump += '_' + ' '
+            nump += '\n'
+        return nump
+    
+    def move_blank(self, direction):
+        '''takes as input a string direction that specifies the 
+        direction in which the blank should move, and that attempts 
+        to modify the contents of the called Board object accordingly
+        '''
+        row = 0
+        col = 0
+        if direction == 'up':
+            row = self.blank_r - 1
+            col = self.blank_c
+            if row > 2 or row < 0:
+                return False
+            else:
+                self.tiles[self.blank_r][self.blank_c] = self.tiles[row][col]
+                self.tiles[row][col] = '0'
+                self.blank_r = row
+                self.blank_c = col
+                return True
+        elif direction == 'down':
+            row = self.blank_r + 1
+            col = self.blank_c
+            if row > 2 or row < 0:
+                return False
+            else:
+                self.tiles[self.blank_r][self.blank_c] = self.tiles[row][col]
+                self.tiles[row][col] = '0'
+                self.blank_r = row
+                self.blank_c = col
+                return True
+        elif direction == 'left':
+            row = self.blank_r
+            col = self.blank_c - 1
+            if col > 2 or col < 0:
+                return False
+            else:
+                self.tiles[self.blank_r][self.blank_c] = self.tiles[row][col]
+                self.tiles[row][col] = '0'
+                self.blank_r = row
+                self.blank_c = col
+                return True
+        elif direction == 'right':
+            row = self.blank_r
+            col = self.blank_c + 1
+            if col > 2 or col < 0:
+                return False
+            else:
+                self.tiles[self.blank_r][self.blank_c] = self.tiles[row][col]
+                self.tiles[row][col] = '0'
+                self.blank_r = row
+                self.blank_c = col
+                return True
+        else:
+            return False
+       
+    def digit_string(self):
+        '''creates and returns a string of digits that corresponds to the 
+        current contents of the called Board object’s tiles attribute
+        '''
+        newstr = ''
+        for r in range(3):
+            for c in range(3):
+                newstr += str(self.tiles[r][c])
+        return newstr
+        
+    def copy(self):
+        '''returns a newly-constructed Board object that is a deep copy of 
+        the called object
+        '''
+        return Board(self.digit_string())
+        
+    def num_misplaced(self):
+        ''' counts and returns the number of tiles in the called Board object 
+        that are not where they should be in the goal state
+        '''
+        count = 0
+        for r in range(3):
+            for c in range(3):
+                self.tiles[r][c] = str(self.tiles[r][c])
+                if self.tiles[r][c] == '0':
+                    count += 0
+                elif self.tiles[r][c] != GOAL_TILES[r][c]:
+                    count += 1
+        return count
+        
+    def __eq__(self, other):
+        '''return True if the called object (self) and the argument (other) 
+        have the same values for the tiles attribute, and False otherwise
+        '''
+        if self.tiles == other.tiles:
+            return True
+        else:
+            return False
+        
+        
+
+
---
+++ searcher.py
@@ -0,0 +1,259 @@
+#
+# searcher.py (Final project)
+#
+# classes for objects that perform state-space search on Eight Puzzles  
+#
+# name: 
+# email:
+#
+# If you worked with a partner, put their contact info below:
+# partner's name:
+# partner's email:
+#
+
+import random
+from state import *
+
+class Searcher:
+    """ A class for objects that perform random state-space
+        search on an Eight Puzzle.
+        This will also be used as a superclass of classes for
+        other state-space search algorithms.
+    """
+    ### Add your Searcher method definitions here. ###
+    def __init__(self, depth_limit):
+        '''constructs a new Searcher object by initializing an attribute 
+        states, an attribute num_tested, an attribute depth_limit
+        '''
+        self.states = []
+        self.num_tested = 0
+        self.depth_limit = depth_limit
+        
+    def add_state(self, new_state):
+        '''takes a single State object called new_state and adds it to the 
+        Searcher‘s list of untested states
+        '''
+        self.states += [new_state]
+        
+    def should_add(self, state):
+        '''takes a State object called state and returns True if the called 
+        Searcher should add state to its list of untested states, and False 
+        otherwise
+        '''
+        if ((self.depth_limit != -1) and (state.num_moves > self.depth_limit))\
+        or (state.creates_cycle()):
+            return False
+        else:
+            return True
+        
+    def __repr__(self):
+        """ returns a string representation of the Searcher object
+            referred to by self.
+        """
+        # You should *NOT* change this method.
+        s = type(self).__name__ + ': '
+        s += str(len(self.states)) + ' untested, '
+        s += str(self.num_tested) + ' tested, '
+        if self.depth_limit == -1:
+            s += 'no depth limit'
+        else:
+            s += 'depth limit = ' + str(self.depth_limit)
+        return s
+    
+    def add_states(self, new_states):
+        '''takes a list State objects called new_states, and that processes 
+        the elements of new_states one at a time as follows
+        '''
+        for i in new_states:
+            if self.should_add(i):
+                self.add_state(i)
+            
+    def next_state(self):
+        '''chooses the next state to be tested from the list of 
+            untested states, removing it from the list and returning it
+        '''
+        s = random.choice(self.states)
+        self.states.remove(s)
+        return s
+    
+    def find_solution(self, init_state):
+        '''performs a full state-space search that begins at the specified 
+        initial state init_state and ends when the goal state is found or 
+        when the Searcher runs out of untested states
+        '''
+        self.add_state(init_state)
+        while self.states != []:
+            self.num_tested += 1
+            s = self.next_state()
+            if s.is_goal():
+                return s
+            else:
+                self.add_states(s.generate_successors())
+        return None
+
+
+
+### Add your BFSeacher and DFSearcher class definitions below. ###
+
+class BFSearcher(Searcher):
+    '''a class for searcher objects that perform breadth-first search (BFS)
+    instead of random search
+    '''
+    
+    def next_state(self):
+        '''remove the chosen state from the list of untested states before 
+        returning it
+        '''
+        s = self.states[0]
+        self.states.remove(s)
+        return s
+
+class DFSearcher(Searcher):
+    '''class for searcher objects that perform depth-first search (DFS) 
+    instead of random search
+    '''
+    
+    def next_state(self):
+        '''follow LIFO (last-in first-out) ordering – choosing the state 
+        that was most recently added to the list
+        '''
+        s = self.states[-1]
+        self.states.remove(s)
+        return s
+
+
+def h0(state):
+    """ a heuristic function that always returns 0 """
+    return 0
+
+### Add your other heuristic functions here. ###
+
+def h1(state):
+    '''takes a State object called state, and that computes and returns an 
+    estimate of how many additional moves are needed to get from state to 
+    the goal state
+    '''
+    return state.board.num_misplaced()    
+
+def h2(self):
+    '''computes and returns an 
+    estimate of how many additional moves are needed to get from state to 
+    the goal state that does a better job than h1
+    '''
+    wrong_row = 0
+    wrong_col = 0
+    count = []
+    '''
+    for i in range(3):
+        if self.board.tiles[0][i] not in '012':
+            wrong_row += 1
+        if self.board.tiles[1][i] not in '345':
+            wrong_row += 1
+        if self.board.tiles[2][i] not in '678':
+            wrong_row += 1
+        if self.board.tiles[i][0] not in '036':
+            wrong_col += 1
+        if self.board.tiles[i][1] not in '147':
+            wrong_col += 1
+        if self.board.tiles[i][2] not in '258':
+            wrong_col += 1
+       '''     
+    for i in range(3):
+        if self.board.tiles[0][i] not in '012':
+            if self.board.tiles[0][i] not in count:
+                count += self.board.tiles[0][i]
+        if self.board.tiles[1][i] not in '345':
+            if self.board.tiles[1][i] not in count:
+                count += self.board.tiles[1][i]
+        if self.board.tiles[2][i] not in '678':
+            if self.board.tiles[2][i] not in count:
+                count += self.board.tiles[2][i]
+        if self.board.tiles[i][0] not in '036':
+            if self.board.tiles[i][0] not in count:
+                count += self.board.tiles[i][0]
+        if self.board.tiles[i][1] not in '147':
+            if self.board.tiles[i][1] not in count:
+                count += self.board.tiles[i][1]
+        if self.board.tiles[i][2] not in '258':
+            if self.board.tiles[i][2] not in count:
+                count += self.board.tiles[i][2]
+        
+    return len(count) -1
+        
+
+class GreedySearcher(Searcher):
+    """ A class for objects that perform an informed greedy state-space
+        search on an Eight Puzzle.
+    """
+    ### Add your GreedySearcher method definitions here. ###
+
+
+    def __repr__(self):
+        """ returns a string representation of the GreedySearcher object
+            referred to by self.
+        """
+        # You should *NOT* change this method.
+        s = type(self).__name__ + ': '
+        s += str(len(self.states)) + ' untested, '
+        s += str(self.num_tested) + ' tested, '
+        s += 'heuristic ' + self.heuristic.__name__
+        return s
+    
+    def __init__(self, heuristic):
+        '''constructs a new GreedySearcher object
+        '''
+        super().__init__(self)
+        self.heuristic = heuristic
+        self.depth_limit = -1
+        
+    def priority(self, state):
+        '''computes and returns the priority of the specified state,
+        based on the heuristic function used by the searcher
+        '''
+        return -1 * self.heuristic(state)
+    
+    def add_state(self, state):
+        '''add a sublist that is a [priority, state] pair, where 
+        priority is the priority of state that is determined by calling the 
+        priority method
+        '''
+        self.states += [[self.priority(state), state]]
+        
+    def next_state(self):
+        '''choose one of the states with the highest priority
+        '''
+        s = max(self.states)
+        self.states.remove(s)
+        return s[-1]
+
+
+
+### Add your AStarSeacher class definition below. ###
+
+class AStarSearcher(GreedySearcher):
+    ''' a subclass of GreedySearcher class for searcher objects that 
+    perform A* search
+    '''
+    
+    def priority(self, state):
+        '''overrides the priority method in teh GreedySearcher class.
+        Computes and returns the priority of the specified state,
+        based on the heuristic function used by the searcher
+        '''
+        return -1 * (self.heuristic(state) + state.num_moves)
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
